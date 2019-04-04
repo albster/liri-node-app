198e5bf4f4fbd8eed1645f38d13dd0953cd3edf1
albster <albster@mac.com>
2019-04-04 18:20:49 -0400

    break on through

---
+++ keys.js
@@ -0,0 +1,17 @@
+console.log('this is loaded');
+
+// Spotify key information
+exports.spotify = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+};
+
+// OMDB information
+exports.OMDB = {
+  id: process.env.OMDB_KEY
+}
+
+
+
+
+
---
+++ liri.js
@@ -0,0 +1,147 @@
+require("dotenv").config();
+// Keys JS file
+const keys = require('./keys');
+// Request details
+const request = require('request');
+// Inquirer details
+const inquirer = require('inquirer');
+// Moment details
+const moment = require('moment');
+// Spotify details
+const Spotify = require('node-spotify-api');
+// Chalk details
+const chalk = require('chalk');
+// Spotify details
+var spotify = new Spotify(keys.spotify);
+// Inquirer prompt
+inquirer.prompt({
+    type: 'list',
+    name: 'choice',
+    message: 'Please select an option: ',
+    choices: ["Find about your favorite bands' concerts", "Get spotify info about songs you like", "Get info about any movie"]
+}).then(liri => {
+    switch (true) {
+        case liri.choice === "Find about your favorite bands' concerts":
+            concertThis();
+            break;
+
+        case liri.choice === "Get spotify info about songs you like":
+            spotifyThisSong();
+            break;
+
+        case liri.choice === "Get info about any movie":
+            movieThis();
+            break;
+
+        case liri.choice === "Get a demo":
+            doWhatItSays();
+            break;
+
+        default:
+
+    }
+    // Function for concerts
+    function concertThis() {
+        inquirer.prompt({
+            type: "input",
+            message: "Please enter an artist?",
+            name: "nameOfArtist"
+        }).then(artistSelection => {
+
+            var url = "https://rest.bandsintown.com/artists/" + artistSelection.nameOfArtist + "/events?app_id=b2b1d13e2f579627ed525e0f00cf2713";
+            console.log(url);
+
+            request(url, (err, res, body) => {
+                if (!err && res.statusCode === 200) {
+                    var obj = JSON.parse(body);
+                    if (body === 'undefined') {
+                        console.log("Please enter a different artist.")
+                    }
+                    else {
+                        for (i = 0; i < obj.length; i++) {
+                            var newTime = obj[i].datetime;
+                            newTime = moment(newTime).format("MM/DD/YYYY");
+                            console.log(`
+==================================================
+${obj[i].lineup} EVENT NUMBER: ${i}
+==================================================
+${chalk.yellow.bold(`* Name of venue: ${obj[i].venue.name}
+* Location of venue: ${obj[i].venue.country}, ${obj[i].venue.city}
+* Date of event: ${newTime}\n`)}`);
+                        }
+                    }
+                }
+            });
+        });
+    }
+    // Spotify function
+    function spotifyThisSong() {
+        inquirer.prompt({
+            type: "input",
+            message: "Enter song: ",
+            name: "songName"
+        }).then(songSelection => {
+            spotify.search({ type: 'track', query: songSelection.songName, limit: 20 }, (err, body) => {
+                if (err) throw err;
+                console.log(`
+=========================================================
+INFO REGARDING THE SONG: ${body.tracks.items[0].name}
+=========================================================
+${chalk.yellow.bold(`* Artist: ${body.tracks.items[0].artists[0].name}
+* Name of song: ${body.tracks.items[0].name}
+* To play the song click on the following link: ${chalk.green.underline(`${body.tracks.items[0].preview_url}`)}
+* The song's album is called: ${body.tracks.items[0].album.name}\n`)}`);
+            });
+            // var urlQuery=`https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx`;
+            //spotify.request(urlQuery)
+            //.then(body=>{
+            //    console.log(body.album.available_markets[0]);
+            //})
+            //.catch.error(`Error occurred: ${err}`);
+            //});
+        });
+    }
+    // Movie function
+    function movieThis() {
+        inquirer.prompt({
+            type: "input",
+            message: "Please enter the movie's name: ",
+            name: "movieName"
+        }).then(movieSelection => {
+            var queryURL = "http://www.omdbapi.com/?t=" + movieSelection.movieName + "&y=&plot=short&apikey=trilogy";
+
+            request(queryURL, (err, res, body) => {
+                if (!err && res.statusCode === 200) {
+                    json = JSON.parse(body);
+                    //console.log(json);
+                    console.log(`
+============================================
+INFORMATION ABOUT: ${json.Title}
+============================================
+${chalk.yellow.bold(`* Year movie came out is: ${json.Year}
+* IMDB movie rating: ${json.imdbRating} 
+* Country of production: ${json.Country}
+* Movie plot: ${json.Plot}
+* Movie Actors: ${json.Actors}\n`)}`);
+                }
+            });
+        });
+
+    }
+    // doWhatItSays function
+    function doWhatItSays() {
+        spotify.search({ type: 'track', query: 'All the Small Things', limit: 20 }, (err, body) => {
+            if (err) throw err;
+            console.log("The title of the movie is : " + err);
+            console.log(body);
+            console.log(`Artist: `);
+            console.log(`Name of song: `);
+            console.log(`To play the song click on the following link: `);
+            console.log(`The song's album is `);
+        });
+
+    }
+
+});
+
+
